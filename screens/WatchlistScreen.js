9141bda20f23826fb8c525244f15b616c7529a30
joepaji <rjoheb@hotmail.com>
2022-12-24 14:37:43 -0700

    setup basic screens

---
+++ screens/WatchlistScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text } from "react-native";
+
+const WatchlistScreen = () => {
+    <View styles={styles.container}>
+        <Text>Watchlist</Text>
+    </View>
+}
+
+export default WatchlistScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+    }
+});
