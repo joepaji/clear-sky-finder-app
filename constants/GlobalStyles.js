dd779bf682ec5d91fef23e20e0f2cea2683051fd
joepaji <rjoheb@hotmail.com>
2022-12-24 14:39:31 -0700

    generic setup code

---
+++ constants/GlobalStyles.js
@@ -0,0 +1,10 @@
+export const GlobalStyles = {
+    colors: {
+        primary800: `#08080D`,
+        primary700: '#151A26',
+        primary500: '#324559',
+        accent300: '#455E73',
+        accent200: '#7791A4',
+        accent100: '#d3e1ee',
+    }
+}
